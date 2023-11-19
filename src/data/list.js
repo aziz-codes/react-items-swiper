ad38de4a563acd68e135719187248f274f97e6fc
aziz-codes <azizisoffline@gmail.com>
2023-11-19 17:46:06 +0500

    created a list of items to be displayed

---
+++ src/data/list.js
@@ -0,0 +1,32 @@
+export const items = [
+  {
+    title: "Buy groceries for the week and make a healthy meal plan",
+    date: "12 Nov",
+    done: false,
+  },
+  {
+    title: "Go for a 5-mile run in the local park and enjoy nature",
+    date: "16 Nov",
+    done: false,
+  },
+  {
+    title: "Read a book from your favorite author for at least an hour",
+    date: "15 Nov",
+    done: false,
+  },
+  {
+    title: "Learn a new programming language or framework for a side project",
+    date: "13 Nov",
+    done: false,
+  },
+  {
+    title: "Write code for a personal project and push it to GitHub",
+    date: "17 Nov",
+    done: false,
+  },
+  {
+    title: "Call a friend you haven't spoken to in a while and catch up",
+    date: "16 Nov",
+    done: false,
+  },
+];
